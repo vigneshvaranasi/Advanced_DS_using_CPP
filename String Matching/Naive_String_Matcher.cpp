3ba758539aeddbc608c9ece603dc07de6b0ce281
vigneshvaranasi <varanasivsv@gmail.com>
2024-04-24 21:04:00 +0530

    s

--- String Matching/Naive_String_Matcher.cpp
+++ String Matching/Naive_String_Matcher.cpp
@@ -5,7 +5,7 @@ void findPattern(string text, string pattern) {
     int n = text.length(), m = pattern.length();
     for (int i = 0; i <= n - m; i++) {
         int j;
-        for (j = 0; j < m; j++) {
+        for (j = 0; j < m; j++) { 
             if (text[i + j] != pattern[j])
                 break;
         }
