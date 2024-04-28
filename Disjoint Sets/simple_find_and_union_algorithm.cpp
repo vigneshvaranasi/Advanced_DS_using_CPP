6fb9633668a413340670ee9ddc941d5fd6405c1f
vigneshvaranasi <varanasivsv@gmail.com>
2024-04-28 11:50:15 +0530

    Refactor findRoot and unionSets functions in simple_find_and_union_algorithm.cpp

--- Disjoint Sets/simple_find_and_union_algorithm.cpp
+++ Disjoint Sets/simple_find_and_union_algorithm.cpp
@@ -1,15 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int findRoot(vector<int>& parent, int x) {
+vector <int> parent;
+
+int findRoot(int x) {
     if (parent[x] != x)
-        parent[x] = findRoot(parent,parent[x]);
+        parent[x] = findRoot(parent[x]);
     return parent[x];
 }
 
-void unionSets(vector<int>& parent, int x, int y) {
-    int xRoot = findRoot(parent, x);
-    int yRoot = findRoot(parent, y);
+void unionSets(int x, int y) {
+    int xRoot = findRoot(x);
+    int yRoot = findRoot(y);
     if (xRoot != yRoot) {
         if (xRoot < yRoot) 
             parent[yRoot] = xRoot;
@@ -22,7 +24,7 @@ int main() {
     int numberOfElements;
     cout << "Enter the number of elements: ";
     cin >> numberOfElements;
-    vector<int> parent(numberOfElements);
+    parent.resize(numberOfElements);
     for (int i = 0; i < numberOfElements; i++) {
         parent[i] = i;
     }
@@ -37,7 +39,7 @@ int main() {
                 int element;
                 cout << "Enter the element: ";
                 cin >> element;
-                int root = findRoot(parent, element);
+                int root = findRoot(element);
                 cout << "Parent of " << element << " is " << root << endl;
                 break;
             }
@@ -45,7 +47,7 @@ int main() {
                 int element1, element2;
                 cout << "Enter two elements to union: ";
                 cin >> element1 >> element2;
-                unionSets(parent, element1, element2);
+                unionSets(element1, element2);
                 cout << "Union performed." << endl;
                 break;
             }
--- Disjoint Sets/union_by_rank_and_path_compression.cpp
+++ Disjoint Sets/union_by_rank_and_path_compression.cpp
@@ -6,14 +6,6 @@ using namespace std;
 vector<int> parent;
 vector<int> ranks;
 
-void initialize(int n) {
-    parent.resize(n);
-    ranks.resize(n, 0);
-    for (int i = 0; i < n; ++i) {
-        parent[i] = i;
-    }
-}
-
 int findPar(int node) {
     if (node == parent[node]) {
         return node;
@@ -21,14 +13,16 @@ int findPar(int node) {
     return parent[node] = findPar(parent[node]); // Path compression
 }
 
-void simpleunion(int u, int v) {
+void simpleUnion(int u, int v) {
     u = findPar(u);
     v = findPar(v);
     if (ranks[u] < ranks[v]) {
         parent[u] = v;
-    } else if (ranks[v] < ranks[u]) {
+    } 
+    else if (ranks[v] < ranks[u]) {
         parent[v] = u;
-    } else {
+    } 
+    else {
         parent[v] = u;
         ranks[u]++;
     }
@@ -38,27 +32,40 @@ int main() {
     int n;
     cout << "Enter the number of vertices: ";
     cin >> n;
+    parent.resize(n);
+    ranks.resize(n, 0);
+    for (int i = 0; i < n; ++i) {
+        parent[i] = i;
+    }
 
-    initialize(n);
-
-    char choice;
+    int choice;
     do {
-        cout << "Enter 'u' for union or 'f' for find operation, or 'q' to quit: ";
+        cout << "\nMenu:\n1. Find\n2. Union\n3. Exit\nEnter your choice: ";
         cin >> choice;
 
-        if (choice == 'u') {
-            int u, v;
-            cout << "Enter values for u and v (separated by space) for union operation: ";
-            cin >> u >> v;
-            simpleunion(u, v);
-        } else if (choice == 'f') {
-            int node;
-            cout << "Enter the value of the node for find operation: ";
-            cin >> node;
-            cout << "Parent of " << node << ": " << findPar(node) << endl;
+        switch (choice) {
+            case 1: {
+                int node;
+                cout << "Enter the value of the node for find operation: ";
+                cin >> node;
+                cout << "Parent of " << node << ": " << findPar(node) << endl;
+                break;
+            }
+            case 2: {
+                int u, v;
+                cout << "Enter values for u and v (separated by space) for union operation: ";
+                cin >> u >> v;
+                simpleUnion(u, v);
+                cout << "Union performed." << endl;
+                break;
+            }
+            case 3:
+                break;
+            default:
+                cout << "Invalid choice. Try again." << endl;
         }
 
-    } while (choice != 'q');
+    } while (choice != 3);
 
     return 0;
 }
